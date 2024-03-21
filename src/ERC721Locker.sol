1178c0e975d67ebf763819ca6bc6cf1785df7445
kphed <kpcrypto69@gmail.com>
2024-03-21 15:24:24 -0400

    Add method for locking ERC721s

--- src/ERC721Locker.sol
+++ src/ERC721Locker.sol
@@ -1,4 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC721Locker {}
+import {ERC721} from "solady/tokens/ERC721.sol";
+
+contract ERC721Locker {
+    struct LockDetails {
+        uint256 id;
+        uint256 expiry;
+    }
+
+    mapping(address owner => mapping(address token => LockDetails)) locks;
+
+    event Lock(
+        address indexed from,
+        address indexed to,
+        address indexed token,
+        uint256 id,
+        uint256 expiry
+    );
+
+    /**
+     * @notice Lock an ERC721 token for a specified duration.
+     * @param  to            address  Account which can withdraw the token after the lock expires.
+     * @param  token         address  ERC721 token contract address.
+     * @param  id            uint256  Token ID.
+     * @param  lockDuration  uint256  Lock duration in seconds (starting from the current timestamp).
+     */
+    function lock(
+        address to,
+        address token,
+        uint256 id,
+        uint256 lockDuration
+    ) external {
+        ERC721(token).transferFrom(msg.sender, address(this), id);
+
+        uint256 expiry = block.timestamp + lockDuration;
+        locks[to][token] = LockDetails({id: id, expiry: expiry});
+
+        emit Lock(msg.sender, to, token, id, expiry);
+    }
+}
